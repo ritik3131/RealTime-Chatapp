c979cee6da3abef13ed98d75321fdf6a41215cb2
OmieSawie <omkarsawant20HG@gmail.com>
2022-10-19 20:09:22 +0530

    Backend is working

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -2,36 +2,49 @@ const messageModel = require("../models/message");
 const roomModel = require("../models/roomModel");
 
 exports.postMessage = async (req, res) => {
-  try {
-    const roomId = req.params.roomId;
-    const messageBody = new messageModel(req.body);
-    await messageBody.save();
-    const room=await roomModel.findById(roomId);
-    room.roomName.messages.push(messageBody);
-    await room.save();
-    res.status(200).json(messageBody);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-};
-
-exports.getMessage=async(req,res)=>{
-    try{
-        const message=await messageModel.find();
-        res.status(200).json(message);
-    }catch(err){
+    try {
+        const roomId = req.params.roomId;
+        const messageBody = new messageModel(req.body);
+        await messageBody.save();
+        const room = await roomModel.findById(roomId);
+		console.log(roomId);
+        room.roomName.messages.push(messageBody);
+        await room.save();
+        res.status(200).json(messageBody);
+    } catch (err) {
         console.log(err);
         res.status(500).send(err);
     }
-}
-exports.getOneMessage=async(req,res)=>{
-  try{
-      const message=await messageModel.find(req.params.messageId);
-      res.status(200).json(message);
-  }catch(err){
-      console.log(err);
-      res.status(500).send(err);
-  }
-}
+};
 
+exports.uploadFile = async (req, res) => {
+    try {
+		console.log(req.params.roomId);
+        const roomId = req.params.roomId;
+        const messageBody = new messageModel(req.body);
+        await messageBody.save();
+        const room = await roomModel.findById(roomId);
+        room.roomName.messages.push(messageBody);
+        await room.save();
+        res.status(200).json(`File Uploaded ${req.file}`);
+    } catch (err) {}
+};
+
+exports.getMessage = async (req, res) => {
+    try {
+        const message = await messageModel.find();
+        res.status(200).json(message);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+};
+exports.getOneMessage = async (req, res) => {
+    try {
+        const message = await messageModel.find(req.params.messageId);
+        res.status(200).json(message);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+};
--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,12 +1,34 @@
-const express=require('express');
-const { postMessage, getMessage, getOneMessage } = require('../controller/messageController');
-const { getCurrentUser } = require('../controller/userController');
+const express = require("express");
+const {
+    postMessage,
+    uploadFile,
+    getMessage,
+    getOneMessage,
+} = require("../controller/messageController");
+const { getCurrentUser } = require("../controller/userController");
 
-const router=express.Router();
+const multer = require('multer');
 
-router.post('/:roomId/new',postMessage);
-router.get('/:roomId/getMessage',getMessage);
-router.get('/user',getCurrentUser);
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "../uploads");
+    },
+	
+    filename: (req, file, callback) => {
+        callback(null, Date.now() + "--" + file.originalname);
+    },
+});
+
+const upload = multer({ storage: fileStorageEngine });
+
+const router = express.Router();
+
+router.post("/:roomId/new", postMessage);
+router.post("/:roomId/uploadFile", upload.single("image"), uploadFile);
+
+
+router.get("/:roomId/getMessage", getMessage);
+router.get("/user", getCurrentUser);
 // router.get('/:messageId',getOneMessage);
 
-module.exports=router;
+module.exports = router;
